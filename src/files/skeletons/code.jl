ce26c7043dcd107230eef949af74813f2dc581d7
likewu <likewu@126.com>
2023-08-17 21:29:02 +0800

    Create code.jl

---
+++ src/files/skeletons/code.jl
@@ -0,0 +1,37 @@
+// import visualization libraries {
+using algorithm-visualizer
+// }
+
+// define tracer variables {
+array2dTracer = Array2DTracer("Grid")
+logTracer = LogTracer("Console")
+// }
+
+// define input variables
+messages = [
+  "Visualize",
+  "your",
+  "own",
+  "code",
+  "here!",
+]
+
+// highlight each line of messages recursively
+function highlight(line)
+  if line >= messages.length return
+  const message = messages[line];
+  // visualize {
+  logTracer.println(message)
+  array2dTracer.selectRow(line, 0, message.length - 1)
+  Tracer.delay();
+  array2dTracer.deselectRow(line, 0, message.length - 1)
+  // }
+  highlight(line + 1)
+end
+
+// visualize {
+Layout.setRoot(VerticalLayout([array2dTracer, logTracer]))
+array2dTracer.set(messages)
+Tracer.delay()
+// }
+highlight(0)
