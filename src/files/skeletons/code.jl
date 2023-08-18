dda32273f2bf2946fd5644eedd94a8efee3e45e6
likewu@126.com <likewu@126.com>
2023-08-18 15:36:42 +0800

    a

--- src/files/skeletons/code.jl
+++ src/files/skeletons/code.jl
@@ -1,13 +1,13 @@
-// import visualization libraries {
+# import visualization libraries {
 using algorithm-visualizer
-// }
+# }
 
-// define tracer variables {
+# define tracer variables {
 array2dTracer = Array2DTracer("Grid")
 logTracer = LogTracer("Console")
-// }
+# }
 
-// define input variables
+# define input variables
 messages = [
   "Visualize",
   "your",
@@ -16,22 +16,22 @@ messages = [
   "here!",
 ]
 
-// highlight each line of messages recursively
+# highlight each line of messages recursively
 function highlight(line)
   if line >= messages.length return
   const message = messages[line];
-  // visualize {
+  # visualize {
   logTracer.println(message)
   array2dTracer.selectRow(line, 0, message.length - 1)
   Tracer.delay();
   array2dTracer.deselectRow(line, 0, message.length - 1)
-  // }
+  # }
   highlight(line + 1)
 end
 
-// visualize {
+# visualize {
 Layout.setRoot(VerticalLayout([array2dTracer, logTracer]))
 array2dTracer.set(messages)
 Tracer.delay()
-// }
+# }
 highlight(0)
